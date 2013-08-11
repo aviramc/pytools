f48c2b610ad527ae02d16d84f559954444577cf3
aviramc <aviram@adallom.com>
2013-08-11 18:14:00 +0300

    Made a package called PyTools.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='PyTools',
+    version='0.1.0',
+    author='aviramc',
+    author_email='',
+    packages=['pytools'],
+    scripts=[],
+    url='https://github.com/aviramc/pytools',
+    license='LICENSE.txt',
+    description='Useful Python extensions',
+    long_description=open('README.md').read(),
+)
